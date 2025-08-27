9f690a6778bf990abfc90a240b15b5e150747072
DorsalTag <78026291+DorsalTag@users.noreply.github.com>
2025-08-27 15:22:12 +0200

    Test Voor Betere Overgang besturings paneel

--- 3D Model Viewer/Assets/UI/3D_Viewer/Scripts/Besturings_Paneel.cs
+++ 3D Model Viewer/Assets/UI/3D_Viewer/Scripts/Besturings_Paneel.cs
@@ -5,6 +5,9 @@ public class Besturings_Paneel : MonoBehaviour
 {
     public Animator Animator;
     public bool hover;
+    public float lerpValuMin = 0f;
+    public float lerpValuMax = 1f;
+    public float lerpValu;
     void Start()
     {
         
@@ -14,12 +17,13 @@ public class Besturings_Paneel : MonoBehaviour
     void Update()
     {
         Animator.SetBool("Hover", hover);
+        Animator.SetFloat ("OpenVal" , lerpValu);
     }
 
     public void HoverOn () {
-        hover= true;
+        lerpValu = Mathf.Lerp (lerpValuMin , lerpValuMax , 0.1f);
     }
     public void HoverOff () {
-        hover= false;
+        lerpValu = Mathf.Lerp (lerpValuMax , lerpValuMin , 0.1f);
     }
 }
